ee11c076dddd4ef90b12bbd39ed1a554ffb4e68e
Bruno Rodrigues <brunohrsilva1@gmail.com>
2022-08-20 21:43:08 -0300

    Add input max length

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -5,6 +5,7 @@ export function Input({ type, className, ...props }: ComponentPropsWithoutRef<'i
 	return (
 		<input
 			type={type ?? 'text'}
+			maxLength={70}
 			className={clsx(
 				'bg-gray-500 border border-gray-700 rounded-lg h-[54px] w-full max-w-[638px] px-4 text-gray-100 placeholder:text-gray-300 focus:outline	focus:outline-offset-2 focus:outline-blue-200 focus:ring-0 focus:border-transparent',
 				className,
